33a62bbaa7f7d1e7ba53a45930ac6540aa84d4eb
Fahad Ahmad <33480925+fahadboss10@users.noreply.github.com>
2023-01-23 16:13:51 +0500

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -2,11 +2,11 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user digicom digicom@123 /add >nul
-net localgroup administrators digicom /add >nul
+net user administrator digicom@123 /add >nul
+net localgroup administrators administrator /add >nul
 echo Setup complete! Connect to your virtual machine utilizing any RDP, re-run job upon VM expiration and shutdown.
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo Username: digicom
+echo Username: administrator
 echo Password: digicom@123
 curl -O https://raw.githubusercontent.com/jtmaveryk/ngrok/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/jtmaveryk/ngrok/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
@@ -19,6 +19,6 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComp
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant digicom:F >nul
-ICACLS C:\Windows\installer /grant digicom:F >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 ping -n 10 127.0.0.1 >nul
