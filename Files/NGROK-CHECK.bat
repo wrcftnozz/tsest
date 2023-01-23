78c027e14bf8465cefe94544c2a3c3e8663c9c2a
Fahad Ahmad <33480925+fahadboss10@users.noreply.github.com>
2023-01-23 16:35:29 +0500

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user admin digicom@123 /add >nul
-net localgroup administrators administrator /add >nul
+net localgroup administrators admin /add >nul
 echo Setup complete! Connect to your virtual machine utilizing any RDP, re-run job upon VM expiration and shutdown.
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo Username: admin
@@ -19,6 +19,6 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComp
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant admin:F >nul
+ICACLS C:\Windows\installer /grant admin:F >nul
 ping -n 10 127.0.0.1 >nul
